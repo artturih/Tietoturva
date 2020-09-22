cb31724f7d28a2b9e356cbfcc2d2dfea92700dec
artturih <61007783+artturih@users.noreply.github.com>
2020-09-22 15:30:04 +0300

    uusien käyttäjien lisääminen AD:hen powershellistä

---
+++ Uusien AD-käyttäjien lisääminen powershellistä.ps1
@@ -0,0 +1,18 @@
+﻿# Luodaan AD-käyttäjiä tiedoston CSV perusteella tai avulla
+
+# kysytään CSV-tiedoston tiedot käyttäjiltä ja tallennetaan ne muuttujaan
+Write-Warning("Luodaan käyttäjätilejä CSV-tiedostosta")
+$Tiedosto = Read-Host("Anna tiedoston nimi ja polku") 
+$Uudetkäyttäjät = Import-Csv -Path $Tiedosto -Delimiter ";" -Encoding Default
+
+# kryptataan ensimmäinen salasana
+$EkaSalasana = ConvertTo-SecureString "MuutaHet1" -AsPlainText -Force
+
+# Määritetään organisaatioyksikkö, johon käyttäjät luodaan
+$Osasto = "OU=Markkinointi, DC=Firma, DC=intra"
+
+# Luodaan AD-käyttäjät
+$Uudetkäyttäjät | New-ADUser -AccountPassword $EkaSalasana -ChangePasswordAtLogon 1 -Enabled 1 -Path $Osasto
+
+$Määrä = $Uudetkäyttäjät.count
+Write-Warning("$määrä käyttäjää lisätty")
